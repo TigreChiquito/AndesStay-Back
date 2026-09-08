0468a11c2abfb690d473df53434659c082d22988
TigreChiquito <bas.cares@duocuc.cl>
2026-09-08 09:20:52 -0300

    Init ms-andesstay-catalog

---
+++ ec2-apps/ms-andesstay-catalog/src/main/java/cl/tigrechiquito/ms_andesstay_catalog/MsAndesstayCatalogApplication.java
@@ -0,0 +1,13 @@
+package cl.tigrechiquito.ms_andesstay_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAndesstayCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAndesstayCatalogApplication.class, args);
+	}
+
+}
